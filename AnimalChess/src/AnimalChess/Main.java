fe49441fa7a4fbbf817944bd80abe6e1f18d1c01
chumingzhang <43573880+chumingzhang@users.noreply.github.com>
2020-02-08 10:20:17 +0800

    Update Main.java

--- AnimalChess/src/AnimalChess/Main.java
+++ AnimalChess/src/AnimalChess/Main.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class Main{
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		frame.setBounds(300, 100, 620, 700);
+		frame.setBounds(300, 100, 540, 700);
 		frame.setTitle("AnimalChess-ZCM");
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
